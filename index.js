9ba3b2483780524e1028e5ec7a0ea3773d7b2249
Rommel <karlsrommel255@gmail.com>
2023-12-13 12:15:13 -0600

    PracticaDeJS0014

---
+++ index.js
@@ -0,0 +1,16 @@
+let nombre = "Juan";
+
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+   }
+
+
+   let compraRealizada = false;
+
+   if (compraRealizada === true) {
+    let subsidioAdicional = 10;
+   } else {
+    let subsidioAdicional = 5;
+   }
+   
